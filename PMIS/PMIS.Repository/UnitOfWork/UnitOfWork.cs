145b076a2f214d6ddff3c99fec787abee85a76d8
Rezwan1994 <rezwan.aiub10@gmail.com>
2022-12-11 10:50:11 +0600

    fixing architecture

---
+++ PMIS/PMIS.Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMIS.Repository.UnitOfWork
+{
+    //internal class UnitOfWork : IUnitOfWork
+    //{
+    //    protected readonly DbContext _dbContext;
+
+    //    public UnitOfWork(DbContext dbContext) => _dbContext = dbContext;
+
+    //    public virtual void Dispose() => _dbContext?.Dispose();
+    //    public virtual void SaveChangesAsync() => _dbContext?.SaveChanges();
+    //}
+}
