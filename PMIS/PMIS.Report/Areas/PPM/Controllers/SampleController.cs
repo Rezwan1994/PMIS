f7c33250333f17bad71fd2a89ad393d1b68596bc
arman <arman.akash20@yahoo.com>
2023-01-10 16:54:37 +0600

    report

---
+++ PMIS/PMIS.Report/Areas/PPM/Controllers/SampleController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Reporting.WebForms;
+using PMIS.Report.Areas.PPM.Data;
+using PMIS.Report.Models;
+using PMIS.Services.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace PMIS.Report.Areas.PPM.Controllers
+{
+    public class SampleController : Controller
+    {
+        private readonly ILogError _logger = new LogError();
+        SampleData _service = new SampleData();
+
+        // GET: PPM/Sample
+        public ActionResult Sample()
+        {
+            try
+            {
+                ReportParams reportParams = new ReportParams();
+                reportParams.COMPANY_ID = 1;
+
+                LocalReport localReport = new LocalReport();
+                localReport.ReportPath = Server.MapPath("~/Areas/PPM/Reports/SampleReport.rdlc");
+
+                ReportDataSource reportDataSource = new ReportDataSource();
+                reportDataSource.Name = "ReportHeader";
+                reportDataSource.Value = _service.GetPageHeaderReport(reportParams);
+                localReport.DataSources.Add(reportDataSource);
+
+                string reportType = "pdf"; //reportParameters.REPORT_EXTENSION;
+                (byte[] renderedBytes, string mimeType) = localReport.RenderReport(reportType);
+                return new FileContentResult(renderedBytes, mimeType);
+            }
+            catch (Exception exp)
+            {
+                _logger.Error(Server, exp);
+                return Content("<h1>Something Went Wrong!</h1>");
+            }
+        }
+    }
+}
---
+++ PMIS/PMIS.Report/Models/QueryPattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PMIS.Report.Models
+{
+    public class QueryPattern
+    {
+        /// <summary>
+        /// SQL Statements
+        /// </summary>
+        public string Query { get; set; }
+
+        /// <summary>
+        /// Parameter List
+        /// </summary>
+        public List<Dictionary<string, string>> Parametes { get; set; } = new List<Dictionary<string, string>>();
+    }
+}
