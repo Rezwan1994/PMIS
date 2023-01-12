b8461f2192a387e3603588a0c8ebf6031538b183
dbaliakat <liakat.ali@squaregroup.com>
2023-01-12 15:29:40 +0600

    service

---
+++ PMIS/PMIS.Service/Implementation/PromotionalProductMaterial/UnitInfoService.cs
@@ -0,0 +1,21 @@
+﻿using PMIS.Domain.Entities;
+using PMIS.Repository.UnitOfWork;
+using PMIS.Service.Interface.PromotionalProductMaterial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMIS.Service.Implementation.PromotionalProductMaterial
+{
+    public class UnitInfoService : BaseService<UNIT_INFO>, IUnitInfoService
+    {
+        public UnitInfoService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            //this._unitOfWork = unitOfWork;
+            //this._categoryRepository = unitOfWork.ICategoryInfoRepository;
+
+        }
+    }
+}
